a1f98992bc764271df604d8401a3a8ae800a9f16
Senorgeno <genedower@gmail.com>
2018-09-28 13:40:30 +1200

    Object to SS_Object

--- code/model/UserDefinedForm.php
+++ code/model/UserDefinedForm.php
@@ -499,7 +499,7 @@ JS
      */
     public function process($data, $form)
     {
-        $submittedForm = Object::create('SubmittedForm');
+        $submittedForm = SS_Object::create('SubmittedForm');
         $submittedForm->SubmittedByID = ($id = Member::currentUserID()) ? $id : 0;
         $submittedForm->ParentID = $this->ID;
 
